f8248ee8b444e8d782f9fc614b4b8f26913fa6b3
Tim Quigg <timjquigg@gmail.com>
2022-10-21 21:51:01 -0600

    finished function to sum an array

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = (array) => {
+  let sum = 0;
+  for (const item of array) {
+
+    // Base case:
+    if (!Array.isArray(item)) {
+      sum += item;
+      continue;
+    }
+    
+    sum += sumItems(item);
+  }
+  return sum;
+};
 
 module.exports = sumItems;
